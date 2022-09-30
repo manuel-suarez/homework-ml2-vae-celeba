eb87e72f606d4d1f86e8962020f21fe71ec1356d
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-30 17:21:47 -0500

    Plot latent space

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ for images in dataset.take(1):
         plt.imshow(images[i].numpy())
         plt.axis('off')
 
-plt.savefig("figure_1.png")
+plt.savefig("figure1.png")
 
 print(images.shape)
 
@@ -371,3 +371,40 @@ vae.fit(dataset,
         steps_per_epoch = steps_per_epoch,
         callbacks       = callbacks)
 vae.save_weights("model_vae_faces_1e4.h5")
+vae2 = VAE(r_loss_factor=R_LOSS_FACTOR)
+vae2.load_weights("best_weight_model.h5")
+import matplotlib.pyplot as plt
+
+def plot_latent_space(vae, input_size=(28,28,1), n=30, figsize=15,  scale=1., latents_start=[0,1]):
+    # display a n*n 2D manifold of digits
+    canvas = np.zeros((input_size[0]*n, input_size[1]*n, input_size[2]))
+    # linearly spaced coordinates corresponding to the 2D plot
+    # of digit classes in the latent space
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    z_sample = np.random.normal(0,1,(1,vae.latent_dim))
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample[0][latents_start[0]], z_sample[0][latents_start[1]]=xi,yi
+            x_decoded = vae.generate(z_sample)
+            img = x_decoded[0].numpy().reshape(input_size)
+            canvas[i*input_size[0] : (i + 1)*input_size[0],
+                   j*input_size[1] : (j + 1)*input_size[1],
+                   : ] = img
+
+    plt.figure(figsize=(figsize, figsize))
+    start_range    = input_size[0] // 2
+    end_range      = n*input_size[0] + start_range
+    pixel_range    = np.arange(start_range, end_range, input_size[0])
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("z[{}]".format(latents_start[0]))
+    plt.ylabel("z[{}]".format(latents_start[1]))
+    plt.imshow(canvas, cmap="Greys_r")
+    plt.savefig("figure2.png")
+
+
+plot_latent_space(vae2, input_size=INPUT_DIM, n = 6, latents_start=[20,30], scale=3)
